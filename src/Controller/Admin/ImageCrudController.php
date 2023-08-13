39a59dc283bf0b924f79a035b889a2e362a43cb8
Yassine NATIJ <yassine.natij@atos.net>
2023-08-13 14:48:18 +0100

    new custom field for ImageCrud + new Field and twg file

--- src/Controller/Admin/ImageCrudController.php
+++ src/Controller/Admin/ImageCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Image;
+use App\Field\VichImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class ImageCrudController extends AbstractCrudController
 {
@@ -12,14 +17,18 @@ class ImageCrudController extends AbstractCrudController
         return Image::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+            IdField::new('id')->hideOnForm(),
+            TextareaField::new('title'),
+            IntegerField::new('size'),
+            VichImageField::new('imageFile'),
+            IntegerField::new('priority'),
+            AssociationField::new('recipe'),
+            AssociationField::new('step'),
+       ];
     }
-    */
+   
 }
